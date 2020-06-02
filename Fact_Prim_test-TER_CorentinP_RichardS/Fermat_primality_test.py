ecc77b68ca0bcc1f2b35b100ec4222c7773a0f56
Corty-coder <59608889+Corty-coder@users.noreply.github.com>
2020-06-02 19:48:21 +0200

    Update Fermat_primality_test.py

--- Fact_Prim_test-TER_CorentinP_RichardS/Fermat_primality_test.py
+++ Fact_Prim_test-TER_CorentinP_RichardS/Fermat_primality_test.py
@@ -2,35 +2,37 @@ from time import time
 from math import *
 import random
 
+def gcd(A,B):
+	'''
+	A and B are integers.
+	'''
+	if A==0 or B==0:
+		return(A if B==0 else B)
+	else:
+		r=A%B
+		B_temp=min(A,B)
+		while r!=0:
+			B_temp=r
+			r=A%B
+			A=B
+			B=r
+		return B_temp
 
-def exponentiation(N): #algorithme pour mettre N-1 sous somme de puissances de 2
-	L=[]
-	N_save=N
-	for k in range(int((log(N,e))/log(2,e)),-1,-1):
-		M=N-2**k
-		if M>=0:
-			N=M
-			L.append(k)
-	a=0
-	return(L)
-
-def expo(a,N):
-	L=exponentiation(N-1)
+def expo_rapide(a,N,modulo):
 	prod=1
-	for i in range(len(L)):
-		c=1
-		n=0
-		gamma=2**L[i]
-		while n<gamma:
-			n+=1
-			c=(a*c)%N
-		prod=(prod*c)%N
-	return(prod)
+	if N==1:
+		return(a)
+	elif N%2==0:
+		c=expo_rapide((a**2)%modulo,N/2,modulo)
+		prod=(prod*c)%modulo
+	elif N%2==1:
+		prod=(prod*a)%modulo
+		c=expo_rapide((a**2)%modulo,(N-1)/2,modulo)
+		prod=(prod*c)%modulo
+	return prod
 
-
-def generator(x,y):
-	return(3*x+y)
-
+def generator(N,x,y):
+	return(int(log2(N))*x+y)
 
 def fermat(N,p): #Fermat
 	N_save=N
@@ -46,40 +48,31 @@ def fermat(N,p): #Fermat
 		while (test==0 and i<w-1): #tant qu'on n'a pas de contre-exemple
 			reset=False
 			i=len(T)
-			a_old=a
-			(b,a)=(a,generator(a,b)%N) #on génère un a
-			while a==0 or a==1 or a_old==a :
-				(b,a)=(a,generator(a,b)%N) #on génère un a
+			a_old=a			
+			(b,a)=(a,generator(N,a,b)%N) #on génère un a
+
+			while a==0 or a==1 or a_old==a or gcd(a,N)!=1:
+				(b,a)=(a,generator(N,a,b)%N) #on génère un a
 
 			if a in T :
 				reset = True
 
 			if not reset :
 				T.append(a)
-				y=expo(a,N)
-			#y=a_puissance_expo(N_save,exponentiation(N),a) #on calcule a**(N-1)%N
+				y=expo_rapide(a,N-1,N) #on calcule a**(N-1)%N
 				if y!=1: #Si a**(N-1)%N != 1, alors le critère est faux.
 					test=1
-	print(T)
+	print("\nNumber 'a' tested :",T)
 	if test==0:
-		return(N, " is a prime or a Carmichael Number with a probability of",p,"%")
+		return(N, " is a prime or a Carmichael Number, with a probability of",p)
 	elif test==1:
 		return(N, " is not a prime")
+
 
 
 if __name__=="__main__" :
 	print("\n\n\tPrimality test based on Fermat's theorem.\n\n")
 	N=int(input("Number to test : "))
-	p=float(input("Exactness desired (i.e. 1-<margin of error>) : "))
+	p=int(input("Exactness desired (i.e. 1-<margin of error>) : "))
 	fermat(N,p)
 
-# a=13451
-# N=3421201
-
-
-# rt=2001911
-
-# z=789002777
-# d=789002775
-
-# print(exponentiation(29))
